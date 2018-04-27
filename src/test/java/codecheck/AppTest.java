cb0f7022c695ba61cedb73a0727b723171ba3aa6
takaichit63 <takaichit63@COAMAC17000166.local>
2018-04-27 16:04:52 +0900

    init commit

---
+++ src/test/java/codecheck/AppTest.java
@@ -0,0 +1,15 @@
+package codecheck;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		App app = new App();
+		assertEquals("a","a");
+	}
+
+}
