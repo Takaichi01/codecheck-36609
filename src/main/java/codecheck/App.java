f36e189c6eb43ee18bef76b21756f80a26b81261
takaichit63 <takaichit63@COAMAC17000166.local>
2018-04-27 17:33:58 +0900

    test実装

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -7,19 +7,32 @@ import javax.ws.rs.client.Client;
 
 public class App {
 	private static Client client = ClientBuilder.newClient();
+	static String seed;
+	static String n;
 	public static void main(String[] args) {
 		
-		WebTarget target = client.target("http://challenge-server.code-check.io");
-//			    .path("/api/recursive")
-//			    .queryParam("n", "3")
-//			    .queryParam("seed", "57a91eaa-6157-4155-a48d-34f07946a803");
-		try {
-		    String result = target.request().get(String.class);
-		    System.out.println(result);
-		} catch (BadRequestException e) {
-//		    logger.error("response=" + e.getResponse().readEntity(String.class), e);
-			System.out.println("response=" + e.getResponse().readEntity(String.class));
-		    throw e;
+		seed = args[0];
+		n = args[1];
+		
+		if (n == "0") {
+			System.out.println("1");
 		}
+		
+		if (n == "1") {
+			System.out.println("2");
+		}
+		
+//		WebTarget target = client.target("http://challenge-server.code-check.io/")
+//			    .path("/api/recursive/ask")
+//			    .queryParam("n", "1")
+//			    .queryParam("seed", "57a91eaa-6157-4155-a48d-34f07946a803");
+//		try {
+//		    String result = target.request().get(String.class);
+//		    System.out.println(result);
+//		} catch (BadRequestException e) {
+////		    logger.error("response=" + e.getResponse().readEntity(String.class), e);
+//			System.out.println("response=" + e.getResponse().readEntity(String.class));
+//		    throw e;
+//		}
 	}
 }
